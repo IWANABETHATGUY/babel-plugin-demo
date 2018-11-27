fc26f9f8b4b9b940e85a45cebfa59914dbc05335
IWANABETHATGUY <974153916@qq.com>
2018-11-27 10:58:29 +0800

    :update 添加生成id函数，可以对应不限量个identifier的情况

--- minify-demo/index.js
+++ minify-demo/index.js
@@ -1,3 +1,70 @@
-const parser = require('@babel/parser');
+let index = 1;
+let varMap = new Map();
+export default function(babel) {
+  const { types: t } = babel;
 
-console.log(ast);
+  return {
+    name: 'ast-transform', // not required
+    visitor: {
+      Identifier(path) {
+        let toVar = varMap.get(path.node.name);
+        if (toVar) {
+          path.node.name = toVar;
+        }
+      },
+      VariableDeclarator(path) {
+        let node = path.node;
+        let toVarRes = getToVar(index);
+        console.log(toVarRes);
+        varMap.set(node.id.name, toVarRes);
+        path.node.id = t.identifier(toVarRes);
+        index++;
+      },
+      MemberExpression(path) {
+        let isVariable = looksLike(path, {
+          parent: p => p === null || p.type !== 'Memberexpression',
+        });
+        if (isVariable) {
+          let name = path.node.object.name;
+          let toVar = varMap.get(name);
+          if (toVar) {
+            path.node.object = t.identifier(toVar);
+          }
+        }
+      },
+    },
+  };
+}
+function getToVar(num) {
+  let list = [...'abcdefghijklmnopqrstuvwxyz'];
+  let res = [];
+  while (num > 0) {
+    let mod = num % 26;
+
+    num = Math.floor(num / 26);
+    if (mod === 0) {
+      mod = 26;
+      num--;
+    }
+    res.unshift(list[mod - 1]);
+  }
+  return res.join('');
+}
+function looksLike(a, b) {
+  return (
+    a &&
+    b &&
+    Object.keys(b).every(bKey => {
+      const bVal = b[bKey];
+      const aVal = a[bKey];
+      if (typeof bVal === 'function') {
+        return bVal(aVal);
+      }
+      return isPrimitive(bVal) ? bVal === aVal : looksLike(aVal, bVal);
+    })
+  );
+}
+
+function isPrimitive(val) {
+  return val == null || /^[sbn]/.test(typeof val);
+}
